6ffd9766e005fa4f4367d9a513fbe790175e4306
Nursultan Bazargaziyev <nursultanbazargaziyev@gmail.com>
2022-02-19 23:24:35 +0100

    Counting Sort algorithm beta

--- Sorting_Algorithms/headerFiles/countingSort.h
+++ Sorting_Algorithms/headerFiles/countingSort.h
@@ -4,13 +4,14 @@
 void countingSort(int *arr, int size, int begin, int last)
 {
     int indexSize = last - begin + 1;
-    int *range = new int[indexSize];
-    for (int i = 0; i < indexSize; i++)
-        range[i] = 0;
-    
+    int* copy = new int[size];
+    int* range = new int[indexSize];
+    for (int i = 0; i < size; i++)
+        copy[i] = arr[i];
 
     for (int i = 0; i < indexSize; i++)
     {
+        range[i] = 0;
         for (int j = 0; j < size; j++) 
             if (begin == arr[j]) 
                 range[i]++;
@@ -18,6 +19,13 @@ void countingSort(int *arr, int size, int begin, int last)
     }
     for (int i = 0; i < indexSize - 1; i++)
         range[i+1] = range[i] + range[i + 1];
+    
+    for (int i = 0; i < size; i++)
+    {
+        int current = range[arr[i]] - 1; 
+        arr[i] = copy[current];
+        range[current]--;
+    }
     
 }
 
